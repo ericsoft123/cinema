f487aeda83e08a1f5975949cc9967171afa5ba91
Eric Kayiranga <ericsoft123@gmail.com>
2020-05-17 04:24:27 +0200

    setup routes on  app.js and import important  package

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-module.exports=function(){
-    return true;
-}
+const express=require('express');
+const mysql=require('mysql');
+const cors=require('cors');
+
+const body_parser=require('body-parser'); //json purpose
+const serverapp=express();
+serverapp.use(express.urlencoded());//you must not forget this
+const port=3000;
+
+serverapp.get('/',(req,res)=>{
+   
+})
+serverapp.listen(port,()=>{
+    console.log("app is running");
+    
+});
