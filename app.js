684ef7da57af8bd92ffbdfbac233d72097f99494
Eric Kayiranga <ericsoft123@gmail.com>
2020-05-17 17:54:39 +0200

    Creating booking routes and code and link to models services codes

--- app.js
+++ app.js
@@ -2,18 +2,68 @@ const express=require('express');
 
 const cors=require('cors');
 
-const body_parser=require('body-parser'); //json purpose
+const body_parser=require('body-parser'); //json get data
 const serverapp=express();
 serverapp.use(express.urlencoded());//you must not forget this
-const db_connect=require('./config/db_config');
+//const db_connect=require('./config/db_config');
 const email_auth_con=require('./config/email_config');
+const my_models=require('./models/services');
 const port=3000;
-var con=db_connect.database_config;
+//var con=db_connect.database_config;
 serverapp.get('/',(req,res)=>{
-   var reslt=email_auth_con.email_auth();
-   //con.end();
-   res.json(reslt.email);
+   
+    var name="erico";
+    var email="ericsoft123@gmail.com";
+    var tel="676867";
+    var seat_id="s-1";
+
+
+ 
+
+
 })
+serverapp.get('/display_seats',(req,res)=>{
+ //
+my_models.main_method["display_seat"](function(result) {//this function display_seat it pass 1 parameters as function to be able to have a call back return values
+    res.json(result);
+  });
+//
+ })
+serverapp.post('/book_seat',(req,res)=>{
+    //
+    book_seat();
+   async function book_seat(){//this to run deleted to make sure if data has been deleted
+     
+//
+var booked=await my_models.main_method["book_seat"](name,email,tel,seat_id);
+     
+
+//res.json(booked.method_name);
+if(booked.result_query)
+{
+var update_seat=await my_models.main_method[booked.method_name](seat_id,booked.status);//note this is update method
+if(update_seat.result_query){
+    var sent_email=await my_models.main_method["sent_email"](name,email,tel,seat_id);
+   if(sent_email.result_query){
+
+   }else{
+       res.json("email is not valid or data is not set");
+   }
+}
+}
+           
+//
+}
+              
+    //
+ })
+serverapp.get('/cancel_seat',(req,res)=>{
+    var reslt=email_auth_con.email_auth();
+    //con.end();
+    var myfunct=my_models.main_method["test"]();
+    res.json(myfunct);
+ })
+
 serverapp.listen(port,()=>{
     console.log("app is running");
     
--- models/services.js
+++ models/services.js
@@ -1,6 +1,11 @@
 
 const db_connect=require('../config/db_config');
+const email_auth_con=require('../config/email_config');
 var con=db_connect.database_config;
+var email_dataconfig=email_auth_con.email_auth();
+var nodemailer = require('nodemailer');
+//
+
 
 
 const result_value=true;//it is true on success only this is to make sure that in case of changing i will change only this valuable
@@ -19,16 +24,33 @@ exports.main_method={//this is a best practise to change functions dynamically
        
     },
     
-    book_seat:function(){//insert data on booking then after call update_seat method and then update methods call sent email(all on async ways)
+     book_seat:function(name,email,tel,seat_id){//insert data on booking then after call update_seat method and then update methods call sent email(all on async ways)
+    
         var sql =`INSERT INTO booking (name,seat_id,email,tel) VALUES ('${name}','${seat_id}','${email}','${tel}')`;
-            con.query(sql, function (err, result) {
-              if (err) throw err;
-              return {
-                result:result_value,
-                method_name:update_method,
-                status:'booked',
-            };
+           return new Promise(function(resolve, reject) {
+            var returnValue = "";
+            con.query(sql, function(error, rows) {
+                if (error) {
+                    returnValue = {
+                        result_query:false,
+                        method_name:update_method,
+                        status:'booked',
+                    };
+                } else {
+                    returnValue ={
+                        result_query:result_value,
+                        method_name:update_method,
+                        status:'booked',
+                    };
+                 
+                }
+                resolve(returnValue)
             });
+        });
+
+        
+      
+      
        
     },
     cancel_seat:function(seat_id){//delete from booking then update_seat status(async ways)
@@ -36,7 +58,7 @@ exports.main_method={//this is a best practise to change functions dynamically
         con.query(sql, function (err, result) {
           if (err) throw err;
           return {
-            result:result_value,
+            result_query:result_value,
             method_name:update_method,
             status:'available',
         };
@@ -46,19 +68,98 @@ exports.main_method={//this is a best practise to change functions dynamically
     update_seat:function(seat_id,status){//every time we book_seat,we will sent email then update or cancel_seat we must  update seat too
         
         var sql =`update seat set status='${status}' where seat_id='${seat_id}'`;//means unbooked
-    con.query(sql, function (err, result) {
-        return {
-            result:result_value,
-            method_name:'sent_email',//only on booking
-           
-        };
-    });
+        return new Promise(function(resolve, reject) {
+            var returnValue = "";
+            con.query(sql, function(error, rows) {
+                if (error) {
+                    returnValue = {
+                        result_query:false,
+                        method_name:update_method,
+                        status:'booked',
+                    };
+                } else {
+                    returnValue ={
+                        result_query:result_value,
+                        method_name:update_method,
+                        status:'booked',
+                    };
+                 
+                }
+                resolve(returnValue)
+            });
+        });
         
     },
-    sent_email:function(){
-        return {
-            result:result_value,
-           // method_name:'',
+    sent_email:function(name,email,tel,seat_id){
+        return new Promise(function(resolve, reject) {
+            var returnValue = "";
+
+//
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user:email_dataconfig.email,
+      pass:email_dataconfig.password
+    }
+  });
+  
+  var mailOptions = {
+    from: 'esoftdev1@gmail.com',
+    to: email,
+    subject: 'Confirmation Booking',
+    html: `Hi Dear ${name}
+    <br>
+    <h3>Booking Details</h3>
+    <table class="demo">
+    
+  
+    <tbody>
+    <tr>
+        <td>Name</td>
+        <td>${name}</td>
+    </tr>
+    <tr>
+        <td>Email</td>
+        <td>${email}</td>
+    </tr>
+    <tr>
+        <td>Tel</td>
+        <td>${tel}</td>
+    </tr>
+    <tr>
+        <td>Seat No</td>
+        <td>${seat_id}</td>
+    </tr>
+    </tbody>
+</table>
+    `
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        returnValue = {
+            result_query:false,
+            method_name:update_method,
+            status:'booked',
         };
+    } else {
+        returnValue ={
+            result_query:result_value,
+            method_name:update_method,
+            status:'booked',
+        };
+    }
+    resolve(returnValue)
+  });
+//
+
+        });
+        
+
+
+        
     },
+    
 }
+
+
